8b06a677364417de35cd4266052cd3a3e46c2841
Michael Walker <Michael.e.walker.87@gmail.com>
2021-07-13 13:28:39 -0600

    Add the Images to the card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.componet'
+import Homepage from './pages/homepage/homepage.componet'
 
 function App() {
   return (
