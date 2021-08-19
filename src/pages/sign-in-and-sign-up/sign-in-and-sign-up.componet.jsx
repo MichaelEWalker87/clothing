c25250e5a5a4a0ce1450fdff3f1503707d376ee8
Michael Walker <Michael.e.walker.87@gmail.com>
2021-08-19 11:40:59 -0600

    Impelement Firebase utlis, included sign up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.componet.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.componet.jsx
@@ -1,11 +1,14 @@
 import React from 'react'; 
+
 import SignIn from '../../components/sign-in/sign-in.componet';
+import SignUp from '../../components/sign-up/sign-up.componet';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn/>
+    <SignUp/> 
   </div>
 )
 export default SignInAndSignUpPage
